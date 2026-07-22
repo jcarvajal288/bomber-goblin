4dfb5911f3fd2cfc5ade71a3e4f9c54b3161030f
Max Carvajal <jcarvajal288@gmail.com>
2026-07-22 16:50:17 -0500

    Explosion areas pulse in and out

--- Bombs/Effects/ExplosionAreas/ExplosionArea3x3/explosion_area_3x_3.gd
+++ Bombs/Effects/ExplosionAreas/ExplosionArea3x3/explosion_area_3x_3.gd
@@ -1,6 +1,20 @@
 extends Node2D
 
+var speed: float = TAU
+var min_alpha: float = 0.2
+var max_alpha: float = 1.0
+
+var time: float = 0.0
+
 
 func tick_up_color() -> void:
 	for sprite in get_children():
 		sprite.frame += 1
+
+
+func _process(delta: float) -> void:
+	time = fmod(time + delta * speed, TAU)	
+	var sine_val: float = (sin(time) + 1.0) / 2.0
+	var new_a = lerp(min_alpha, max_alpha, sine_val)
+	for sprite in get_children():
+		sprite.modulate.a = new_a
