46e310daa0a6aee862b0e3cf3e291ead8e2c9fa0
Max Carvajal <jcarvajal288@gmail.com>
2026-07-22 15:58:37 -0500

    Snap bomb spawning to grid

--- Bombs/BombSpawner/bomb_spawner.gd
+++ Bombs/BombSpawner/bomb_spawner.gd
@@ -8,6 +8,18 @@ func _ready() -> void:
 
 
 func _spawn_bomb(bomb_position: Vector2) -> void:
+	var player_position = Global.player.global_position
 	var bomb = BLACK_BOMB_SCENE.instantiate()
-	bomb.global_position = bomb_position
+	var grid_x = snapped(bomb_position.x, 8)
+	var grid_y = snapped(bomb_position.y, 8)
+	if (grid_x <= player_position.x):
+		grid_x += 4
+	else:
+		grid_x -= 4
+	if (grid_y <= player_position.y):
+		grid_y += 4
+	else:
+		grid_y -= 4
+	var grid_snapped = Vector2(grid_x, grid_y)
+	bomb.global_position = grid_snapped
 	add_child(bomb)
