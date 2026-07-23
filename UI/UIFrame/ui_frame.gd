75421ebfed59cf32b513bde576b231573e357856
Max Carvajal <jcarvajal288@gmail.com>
2026-07-23 10:50:38 -0500

    Stage and Enemy timers tick down

---
+++ UI/UIFrame/ui_frame.gd
@@ -0,0 +1,11 @@
+extends MarginContainer
+
+@onready var score_label: Label = $PanelContainer/ScoreMarginContainer/ScoreLabel
+@onready var chests_label: Label = $PanelContainer/ChestsMarginContainer/ChestsLabel
+@onready var timer_label: Label = $PanelContainer/TimerMarginContainer/TimerLabel
+@onready var enemy_label: Label = $PanelContainer/EnemyMarginContainer/EnemyLabel
+
+
+func _process(_delta: float) -> void:
+	timer_label.text = "%03d" % $StageTimer.time_left
+	enemy_label.text = "%02d" % $EnemyTimer.time_left
