b13cd08a7a797b691a97dbca1f93dc03d554c8c3
Max Carvajal <jcarvajal288@gmail.com>
2026-07-23 09:48:09 -0500

    Add Bomb Preview to UI Frame

--- UI/BombPreview/bomb_preview.gd
+++ UI/BombPreview/bomb_preview.gd
@@ -1,7 +1,7 @@
-extends MarginContainer
+extends TextureRect
 
-const FRAME_WIDTH = 41
-const FRAME_HEIGHT = 35
+const FRAME_WIDTH = 56
+const FRAME_HEIGHT = 56
 
 var next_bomb_shape: int = 0
 
@@ -12,6 +12,6 @@ func _ready() -> void:
 
 func change_next_bomb_shape(new_area: int) -> void:
 	next_bomb_shape = new_area
-	var atlas_tex = $PanelContainer/TextureRect.texture as AtlasTexture
+	var atlas_tex = texture as AtlasTexture
 	var new_x = next_bomb_shape * FRAME_WIDTH
 	atlas_tex.region = Rect2(new_x, 0, FRAME_WIDTH, FRAME_HEIGHT)
