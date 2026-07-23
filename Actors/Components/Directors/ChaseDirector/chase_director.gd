1442b3d83bfb04899433d311a9d1386e95bac016
Max Carvajal <jcarvajal288@gmail.com>
2026-07-23 16:07:11 -0500

    Fix Dwarf Fighter and make him worth score

--- Actors/Components/Directors/ChaseDirector/chase_director.gd
+++ Actors/Components/Directors/ChaseDirector/chase_director.gd
@@ -1,5 +1,7 @@
 extends Director
 
+const attack_distance = 20
+
 
 func _process(_delta: float) -> void:
 	var destination = Global.player.global_position
--- Actors/DwarfFighter/dwarf_fighter.gd
+++ Actors/DwarfFighter/dwarf_fighter.gd
@@ -1,7 +1,16 @@
 extends CharacterBody2D
 
-const speed = 40
+const speed = 25
+
+@export var data: Resource
+
+@onready var has_exploded = false
 
 
 func _ready() -> void:
 	$StateMachine.init(self)
+
+
+func on_explosion(multiplier: int) -> void:
+	ScoreTracker.add_score.emit(data.base_value, global_position, multiplier)
+	$StateMachine.change_state($StateMachine/Die)
