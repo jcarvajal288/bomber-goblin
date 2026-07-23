74b0f560f889244b8c5368674cddeae4f1d83ca3
Max Carvajal <jcarvajal288@gmail.com>
2026-07-23 00:10:05 -0500

    Replace bomb preview window

--- UI/BombPreview/bomb_preview.gd
+++ UI/BombPreview/bomb_preview.gd
@@ -1,6 +1,7 @@
 extends MarginContainer
 
-const FRAME_WIDTH = 49
+const FRAME_WIDTH = 41
+const FRAME_HEIGHT = 35
 
 var next_bomb_shape: int = 0
 
@@ -13,4 +14,4 @@ func change_next_bomb_shape(new_area: int) -> void:
 	next_bomb_shape = new_area
 	var atlas_tex = $PanelContainer/TextureRect.texture as AtlasTexture
 	var new_x = next_bomb_shape * FRAME_WIDTH
-	atlas_tex.region = Rect2(new_x, 0, FRAME_WIDTH, FRAME_WIDTH)
+	atlas_tex.region = Rect2(new_x, 0, FRAME_WIDTH, FRAME_HEIGHT)
