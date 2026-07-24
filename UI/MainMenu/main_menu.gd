856336a4b6daba936282a1fe128479681625f7fd
Max Carvajal <jcarvajal288@gmail.com>
2026-07-24 14:56:06 -0500

    Start in Main Menu and can start and quit the game

---
+++ UI/MainMenu/main_menu.gd
@@ -0,0 +1,45 @@
+extends CanvasLayer
+
+var selection: int = 0
+
+const GAMEPLAY_SCENE: PackedScene = preload("res://gameplay.tscn")
+
+@onready var start_option = $MenuOptionsContainer/Start
+@onready var quit_option = $MenuOptionsContainer/Quit
+
+@onready var menu_options = [
+	start_option,
+	quit_option
+]
+
+func _ready() -> void:
+	menu_options[selection].modulate = Color.YELLOW
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_down"):
+		selection = (selection + 1) % len(menu_options)
+		change_selection()
+	elif Input.is_action_just_pressed("ui_up"):
+		selection = (selection - 1) % len(menu_options)
+		change_selection()
+	elif Input.is_action_just_pressed("ui_accept"):
+		perform_action()
+
+
+func change_selection() -> void:
+	for option in menu_options:
+		option.modulate = Color.WHITE
+	menu_options[selection].modulate = Color.YELLOW
+
+
+func perform_action() -> void:
+	if selection == 0:
+		start_game()
+	elif selection == 1:
+		get_tree().quit()
+
+
+func start_game() -> void:
+	get_tree().change_scene_to_packed(GAMEPLAY_SCENE)
+		
