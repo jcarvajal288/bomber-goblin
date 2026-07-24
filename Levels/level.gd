73f68f67c746094cd0e0c447d907affa3de4f417
Max Carvajal <jcarvajal288@gmail.com>
2026-07-24 10:11:51 -0500

    Implement player start marker

--- Levels/level.gd
+++ Levels/level.gd
@@ -2,15 +2,24 @@ extends Node
 
 const PLAYER_SCENE = preload("res://Actors/Sapper/Sapper.tscn")
 
+var starting_location: Vector2 = Vector2.ZERO
+
 
 func _ready() -> void:
 	Global.respawn_player.connect(_respawn_player)
+	starting_location = $PlayerStartMarker.global_position
+	$PlayerStartMarker.queue_free()
+	spawn_player()
+
+
+func spawn_player() -> void:
+	var player = PLAYER_SCENE.instantiate()
+	player.global_position = starting_location
+	Global.player = player
+	add_child(player)
 
 
 func _respawn_player() -> void:
-	var player = PLAYER_SCENE.instantiate()
-	player.global_position = Vector2(265, 238)
-	Global.player = player
-	add_child(player)
+	spawn_player()
 	var score_deduction = ScoreTracker.score * 0.1
-	ScoreTracker.add_score.emit(score_deduction, player.global_position, -1)
+	ScoreTracker.add_score.emit(score_deduction, Global.player.global_position, -1)
